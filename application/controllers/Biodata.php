9b3d12d4da235a0ad67917b49c70f4442fa6303c
voodoo875 <rizkyawaludin875@gmail.com>
2023-03-30 17:09:07 +0700

    Tugas2

---
+++ application/controllers/Biodata.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Biodata extends CI_Controller {
+
+    public function biodataku()
+	{
+		$this->load->view('biodata');
+	}
+}
---
+++ application/views/biodata.php
@@ -0,0 +1,4 @@
+<H1> Biodata<H1>
+<h2> Rizky Awaludin (17200421)</h2>
+<h2>Kelas   :17.6A.26 </h2>
+
