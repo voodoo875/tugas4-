3980c5cea71b7b5470be88538d7e517defb234d7
voodoo875 <rizkyawaludin875@gmail.com>
2023-04-01 01:31:29 +0700

    tugaspro

--- application/controllers/Biodata.php
+++ application/controllers/Biodata.php
@@ -7,4 +7,10 @@ class Biodata extends CI_Controller {
 	{
 		$this->load->view('biodata');
 	}
+
+	public function penjumlahan($nilai1, $nilai2)
+	{
+		echo $this->Model_aritmatika->penjumlahan($nilai1, $nilai2);
+	}
+
 }
